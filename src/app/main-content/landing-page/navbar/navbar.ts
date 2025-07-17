b367f7d22f3c97d3afa6f15ec2c4a4290f280f74
Anne Vollmann <vollmann.anne@web.de>
2025-07-17 11:40:09 +0200

    navbar finished
    - navbar links generated with *ngFor
    - nav-shape as img
    - hrefs work

--- src/app/main-content/landing-page/navbar/navbar.ts
+++ src/app/main-content/landing-page/navbar/navbar.ts
@@ -1,17 +1,29 @@
-import { NgClass } from '@angular/common';
+import { CommonModule, NgClass, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [NgClass],
+  imports: [NgClass, CommonModule, NgStyle],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
 export class Navbar {
+  navBtns = [
+    {section: 'About me', href: '#about'},
+    {section: 'Skills', href: '#skills'},
+    {section: 'Projects', href: '#projects'},
+    {section: 'Contact', href: '#contact'},
+  ];
+
   languageBtnHovered = false;
   currentLanguage = 'DE';
+  currentSection = '';
 
-  setLanguage(language:string) {
+  setLanguage(language: string) {
     this.currentLanguage = language;
+  }
+
+  setSection(section: string) {
+    this.currentSection = section;
   }
 }
