9ef69b8f6fac8aef171114573338fdc96f697f22
Anne Vollmann <vollmann.anne@web.de>
2025-07-17 13:34:57 +0200

    my skills nearly finished
    - icons for each learned skill
    - currently learning-area: basic structure
    
    - about me: img-alt added

--- src/app/main-content/my-skills/my-skills.ts
+++ src/app/main-content/my-skills/my-skills.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-my-skills',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-skills.html',
   styleUrl: './my-skills.scss'
 })
 export class MySkills {
-
+  skillIcons = [
+    {src: 'assets/icons/icon_angular.svg', alt: 'Angular'},
+    {src: 'assets/icons/icon_typescript.svg', alt: 'TypeScript'},
+    {src: 'assets/icons/icon_javascript.svg', alt: 'JavaScript'},
+    {src: 'assets/icons/icon_html.svg', alt: 'HTML'},
+    {src: 'assets/icons/icon_css.svg', alt: 'CSS'},
+    {src: 'assets/icons/icon_restapi.svg', alt: 'REST-API'},
+    {src: 'assets/icons/icon_firebase.svg', alt: 'Firebase'},
+    {src: 'assets/icons/icon_git.svg', alt: 'Git'},
+    {src: 'assets/icons/icon_materialdesign.svg', alt: 'Material Design'},
+    {src: 'assets/icons/icon_scrum.svg', alt: 'Scrum'},
+  ]
 }
