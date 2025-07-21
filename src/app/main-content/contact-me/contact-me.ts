65ac4df5d1a259140576573c397fd02493dddaa8
Anne Vollmann <vollmann.anne@web.de>
2025-07-21 12:21:14 +0200

    contact-fom basic structure

--- src/app/main-content/contact-me/contact-me.ts
+++ src/app/main-content/contact-me/contact-me.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact-me',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contact-me.html',
   styleUrl: './contact-me.scss'
 })
 export class ContactMe {
+  mailHovered = false;
+  phoneHovered = false;
+  checkboxHovered = false;
+  checkboxChecked = false;
 
+  checkInput(id: string) {
+    return;
+  }
+
+  submitMessage() {
+    return;
+  }
 }
