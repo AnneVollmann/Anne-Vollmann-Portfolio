f29c087ec2a0e78acbf75e76773e363336620157
Anne Vollmann <vollmann.anne@web.de>
2025-07-21 13:59:18 +0200

    footer finished, social-btns effects simplified

--- src/app/main-content/footer/footer.ts
+++ src/app/main-content/footer/footer.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss'
 })
 export class Footer {
+  socialBtns = [
+    { srcBase: 'assets/icons/button_mail', alt: 'mailBtn', hover: false},
+    { srcBase: 'assets/icons/button_github', alt: 'githubBtn', hover: false},
+    { srcBase: 'assets/icons/button_linkedin', alt: 'linkedinBtn', hover: false}
+  ];
 
+  setSocialBtnHovered(btn: any, hover: boolean) {
+    btn.src = `assets/icons/${btn.base}${hover ? '_hover' : ''}.svg`;
+  }
 }
--- src/app/main-content/landing-page/landing-page.ts
+++ src/app/main-content/landing-page/landing-page.ts
@@ -10,17 +10,11 @@ import { CommonModule } from '@angular/common';
 
 export class LandingPage {
   socialBtns = [
-    { src: 'assets/icons/button_mail.svg', alt: 'button mail', base: 'button_mail' },
-    { src: 'assets/icons/button_github.svg', alt: 'button github', base: 'button_github' },
-    { src: 'assets/icons/button_linkedin.svg', alt: 'button linkedin', base: 'button_linkedin' }
+    { srcBase: 'assets/icons/button_mail_blue', alt: 'mailBtn', hover: false},
+    { srcBase: 'assets/icons/button_github_blue', alt: 'githubBtn', hover: false},
+    { srcBase: 'assets/icons/button_linkedin_blue', alt: 'linkedinBtn', hover: false}
   ];
-
   arrowBtnHovered = false;
-
-
-  setSocialBtnHovered(btn: any, hover: boolean) {
-    btn.src = `assets/icons/${btn.base}${hover ? '_hover' : ''}.svg`;
-  }
 
   //ggf. global
   setArrowBtnHovered(hover: boolean) {
