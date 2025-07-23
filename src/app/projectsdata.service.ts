0358d739905bf3a39ba0f76d6be4b3d521bd552d
Anne Vollmann <vollmann.anne@web.de>
2025-07-23 11:14:16 +0200

    logo added, navbar overhauled, small text adjustments (about me, contact)
    - navbar elements 100% height and width
    - shape as bg-img
    - class for current-section-btn

--- src/app/projectsdata.service.ts
+++ src/app/projectsdata.service.ts
@@ -10,8 +10,8 @@ export class ProjectsdataService {
     projects = [
         {
             title: 'Join',
-            aboutDe: 'Join ist ein Task-Managementsystem inspiriert vom Kanban-System. Es können Aufgaben erstellt, bearbeitet, organisiert und gelöscht werden. Dafür gibt es eine Drag- and Drop-Funktion und User und Kategorien können zugeordnet werden. Außerdem können User sich ein eigenes Profil anlegen.',
-            aboutEn: 'Join is a task management system inspired by the Kanban method. Tasks can be created, edited, organized, and deleted. It features a drag-and-drop function, and users and categories can be assigned to tasks. Additionally, users can create their own profile.',
+            aboutDe: 'Join ist ein Task-Manager inspiriert vom Kanban-System. Es können Aufgaben erstellt, bearbeitet, organisiert und gelöscht werden. Dafür gibt es eine Drag- and Drop-Funktion und Kontakte und Kategorien können zugeordnet werden. Außerdem können User sich ein eigenes Profil anlegen.',
+            aboutEn: 'Join is a task manager inspired by the Kanban method. Tasks can be created, edited, organized, and deleted. It features a drag-and-drop function, and contacts and categories can be assigned to tasks. Additionally, users can create their own profile.',
             durationWeeks: '6',
             organizationDe: 'Schon vor Beginn haben wir klare Regeln zur Einheitlichkeit des Codes aufgestellt (z.B. zur Bennennung von Funktionen und Abbildungen). Wir haben das Projekt erst grob in Teilaufgaben untergliedert, welche wir dann in einzelne Tasks mit konkreten Deadlines teilten. In regelmäßigen Videocalls tauschten wir uns aus und besprachen Ideen und Probleme.',
             organizationEn: 'Even before starting, we established clear rules to ensure code consistency (e.g., for naming functions and images). We initially divided the project into broad sub-tasks, which we then broke down into individual tasks with specific deadlines. In regular video calls, we exchanged ideas and discussed problems.',
@@ -26,7 +26,7 @@ export class ProjectsdataService {
         },
         {
             title: 'Dusky Swamps',
-            aboutDe: 'Dusky Swamps ist ein kleines Jump-an-Run Spiel mit Fokus auf objekt-orientierte Programmierung. Als Astronaut besiegt man die Monster des Sumpfes, sammelt Edelsteine und kämpft gegen einen Endboss.',
+            aboutDe: 'Dusky Swamps ist ein kleines Jump-and-Run Spiel mit Fokus auf objekt-orientierte Programmierung. Als Astronaut besiegt man die Monster des Sumpfes, sammelt Edelsteine und kämpft gegen einen Endboss.',
             aboutEn: 'Dusky Swamps is a small jump-and-run game with a focus on object-oriented programming. As an astronaut, you defeat the swamp monsters, collect gems, and fight a final boss.',
             durationWeeks: '6',
             organizationDe: 'Eine eindeutige Benennung von Klassen und Methoden, sowie eine verständliche Dokumentation sind essentielle Bestandteile dieses Projekts. Regelmäßige Tests und Anpassungen waren nötig, um eine spaßige Spielerfahrung zu bieten.',
