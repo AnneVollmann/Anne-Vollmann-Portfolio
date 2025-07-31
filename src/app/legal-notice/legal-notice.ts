c06263bc8c028677f475efd49836b59d31b2f872
Anne Vollmann <vollmann.anne@web.de>
2025-07-31 09:44:54 +0200

    feedback implemented
    - legals new html-structure
    - landing page always 100vh
    - references got a little more space

--- src/app/legal-notice/legal-notice.ts
+++ src/app/legal-notice/legal-notice.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { LanguageAwareBase } from '../shared/base/language-aware.base';
 import { LanguageService } from '../shared/services/language.service';
+import { Navbar } from "../main-content/navbar/navbar";
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [CommonModule],
+  imports: [CommonModule, Navbar],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss'
 })
